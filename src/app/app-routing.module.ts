47eae8f0bfc526f533603298469fe217c662190e
mecustodio <mecustodio@hotmail.es>
2020-01-05 17:30:52 +0100

    sass 1.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
-import { HomeComponent } from "./pages/home/home.component";
-import { UsersComponent } from "./pages/users/users.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { UsersComponent } from './pages/users/users.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "/users", component: UsersComponent },
-  { path: "**", pathMatch: "full", redirectTo: "home" }
-  
+  { path: '', component: HomeComponent },
+  { path: './users', component: UsersComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+
 ];
 
 @NgModule({
